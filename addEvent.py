2d0fb9f30cfef770aa978dadceb5c5136aa54c9d
abhishekshah212 <ass014@latech.edu>
2016-04-03 13:19:17 +0000

    Basic functionality with regex text processing done

--- addEvent.py
+++ addEvent.py
@@ -1,7 +1,8 @@
 from __future__ import print_function
 import httplib2
 import os
-
+import re, logging, Image, pytesseract, datetime
+from PIL import Image
 from apiclient import discovery
 import oauth2client
 from oauth2client import client
@@ -54,6 +55,178 @@ def get_credentials():
         print('Storing credentials to ' + credential_path)
     return credentials
 
+
+def processDate(data):
+    copy_of_data = data
+    # print data
+    '''
+    keywordRegex = re.compile(r'(date|time)*')
+    matching_keyword_objects = keywordRegex.search(data)
+    print matching_keyword_objects.group()
+    
+    dateRegex = re.compile(r'((January|February|March|April|May|June|July|August|September|October|November|December|JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER)+.*\d\d)')
+    matching_date_list = dateRegex.findall(data)
+    print matching_date_list
+    # first index is the best one (most specific)
+    single_date = matching_date_list[0][0]
+    print single_date
+    
+    # NEED TO CONVERT DATE/TIME INTO RFC3339 FORMAT
+    '''
+    dateRegex = re.compile(r'((?:(?<!\:)(?<!\:\d)[0-3]?\d(?:st|nd|rd|th)?\s+(?:of\s+)?(?:jan\.?|january|feb\.?|february|mar\.?|march|apr\.?|april|may|jun\.?|june|jul\.?|july|aug\.?|august|sep\.?|september|oct\.?|october|nov\.?|november|dec\.?|december)|(?:jan\.?|january|feb\.?|february|mar\.?|march|apr\.?|april|may|jun\.?|june|jul\.?|july|aug\.?|august|sep\.?|september|oct\.?|october|nov\.?|november|dec\.?|december)\s+(?<!\:)(?<!\:\d)[0-3]?\d(?:st|nd|rd|th)?)(?:\,)?\s*(?:\d{4})?|[0-3]?\d[-\./][0-3]?\d[-\./]\d{2,4})', re.IGNORECASE)
+    matching_date_list = dateRegex.findall(data)
+    # print matching_date_list
+    single_date = matching_date_list[0].replace("\n", "")
+    # print single_date
+    
+    monthDict={1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun', 7:'Jul', 8:'Aug', 9:'Sep', 10:'Oct', 11:'Nov', 12:'Dec'}
+    monthDict = {"january":1, "february": 2, "march": 3, "april": 4, "may": 5, "june": 6, "july": 7, "august": 8, "september": 9, "october": 10, "november": 11, "december": 12}
+    split_date = single_date.split()
+    date = split_date[1]
+    month = monthDict[split_date[0].lower()]
+    
+    # single value months (0-9)
+    if len(str(month)) < 2:
+        month = '0' + str(month)
+    # print "date", date
+    # print "month", month
+    
+    '''
+    # time regex
+    timeRegex = re.compile(r'(\d.*(PM|AM))')
+    
+    matching_time_list = timeRegex.findall(data)
+    print "0"
+    print matching_time_list
+    
+    time_span = matching_time_list[0][0]
+    try:
+        startTime = time_span.split('-')
+        time_span = time_span.split('-')
+    except:
+        startTime = time_span.split('')
+    
+    print time_span
+    '''
+    # time regex
+    timeRegex = re.compile(r'(\d{1,2}:\d{2} ?(?:[ap]\.?m\.?)?|\d[ap]\.?m\.?)', re.IGNORECASE)
+    
+    time_span = timeRegex.findall(data)
+    
+    # print "0"
+    # print time_span
+
+    
+    
+    # appropiate start and end times
+    if len(time_span) == 2:
+        startTime = time_span[0]
+        endTime = time_span[1]
+    else:
+        startTime = time_span[0]
+        endTime = None
+    
+    # print startTime
+    # print endTime
+    
+    # begin to convert to rfc 3339 format
+    # NEED TO ADD THE ABILITY TO HAVE MINUTES IN THE PROCESSING (SPLIT ON :)
+    if 'pm' in startTime.lower():
+
+        initial_time = int(startTime.lower().replace("pm", ""))
+        initial_time = (initial_time + 12) % 24
+        
+        # single value months (0-9)
+        if len(str(initial_time)) < 2:
+            month = '0' + str(initial_time)
+        # print initial_time
+        #initial_time_number = int(initial_time.split())
+        
+    elif 'am' in startTime.lower():
+        initial_time = startTime.lower().replace("am", "")
+        # single value months (0-9)
+        if len(str(initial_time)) < 2:
+            month = '0' + str(initial_time)
+        # print initial_time
+        
+    else:
+        pass
+    
+    # end time
+    if 'pm' in endTime.lower():
+
+        end_time = int(endTime.lower().replace("pm", ""))
+        end_time = (end_time + 12) % 24
+        
+        # single value months (0-9)
+        if len(str(end_time)) < 2:
+            month = '0' + str(end_time)
+        # print end_time
+        #end_time_number = int(end_time.split())
+        
+    elif 'am' in endTime.lower():
+        end_time = endTime.lower().replace("am", "")
+        # single value months (0-9)
+        if len(str(end_time)) < 2:
+            month = '0' + str(end_time)
+        # print end_time
+        
+    else:
+        pass
+    
+    
+    
+    # hardcoded the year
+    # year, month, day, time, minute, month
+    try:
+        start_date_rfc = datetime.datetime(2016, int(month), int(date), int(initial_time), 00, 00).strftime("%Y-%m-%dT%I:%M:%S-05:00")
+    except:
+        start_date_rfc = datetime.datetime(2016, int(month), int(date), int(initial_time), 00, 00).strftime("%Y-%m-%dT%I:%M:%S-05:00")
+        
+    
+    # print(start_date_rfc) 
+    
+    #2016-05-28T09:00:00-05:00
+    
+    # end date
+    try:
+        end_date_rfc = datetime.datetime(2016, int(month),int(date), int(end_time), 00, 00).strftime("%Y-%m-%dT%I:%M:%S-05:00")
+    except:
+        end_date_rfc = datetime.datetime(2016, int(month),int(date), int(end_time), 00, 00).strftime("%Y-%m-%dT%I:%M:%S-05:00")
+    
+    # print end_date_rfc
+    
+    event['start']['dateTime'] = start_date_rfc
+    event['end']['dateTime'] = end_date_rfc
+    
+def processDescription(data):
+    numberRegex = re.compile(r'(((?:(?<![\d-])(?:\+?\d{1,3}[-.\s*]?)?(?:\(?\d{3}\)?[-.\s*]?)?\d{3}[-.\s*]?\d{4}(?![\d-]))|(?:(?<![\d-])(?:(?:\(\+?\d{2}\))|(?:\+?\d{2}))\s*\d{2}\s*\d{3}\s*\d{4}(?![\d-]))))', re.IGNORECASE)
+    matching_number_list = numberRegex.findall(data)
+    # print matching_number_list
+    event['description'] = matching_number_list[0][0]
+
+  
+def processLocation(data):
+    addressRegex = re.compile(r'(\d{1,4}[\w\s]{1,20}(?:street|st|avenue|ave|road|rd|highway|hwy|square|sq|trail|trl|drive|dr|court|ct|parkway|pkwy|circle|cir|boulevard|blvd)?.*[0-9]{5}(-[0-9]{4})?)', re.IGNORECASE)
+    matching_address_list = addressRegex.findall(data)
+    # print data
+    # print 'lol'
+    # print matching_address_list
+    event['location'] = matching_address_list[0][0]
+    
+def processText(PlainText):
+    
+    processDate(PlainText)
+    print '--------'
+    processLocation(PlainText)
+    # title = processTitle(PlainText)
+    print '+++++++++++'
+    processDescription(PlainText)
+
+
+processText(PlainText)
+
+
 def main():
 	"""Shows basic usage of the Google Calendar API.
 	
@@ -65,29 +238,19 @@ def main():
 	service = discovery.build('calendar', 'v3', http=http)
 	
 	now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-	print('Getting the upcoming 10 events')
-	eventsResult = service.events().list(
-	calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
-	orderBy='startTime').execute()
-	events = eventsResult.get('items', [])
-	
-	if not events:
-		print('No upcoming events found.')
-		for event in events:
-			start = event['start'].get('dateTime', event['start'].get('date'))
-			print(start, event['summary'])
+
 	
 	
 	event = {
-	  'summary': str(title),
-	  'location': '800 Howard St., San Francisco, CA 94103',
-	  'description': 'A chance to hear more about Google\'s developer products.',
+	  'summary': 'I was unable to get a summary.',
+	  'location': 'Unable to find a location',
+	  'description': 'I was unable to find a description.',
 	  'start': {
-	    'dateTime': '2016-05-28T09:00:00-05:00',
+	    'dateTime': '2016-04-20T09:00:00-05:00',
 	    'timeZone': 'America/Chicago',
 	  },
 	  'end': {
-	    'dateTime': '2016-05-28T17:00:00-05:00',
+	    'dateTime': '2016-04-20T17:00:00-05:00',
 	    'timeZone': 'America/Chicago',
 	  },
 	  'recurrence': [
@@ -106,11 +269,11 @@ def main():
 	}
 	
 
-
+	processText(PlainText)
 
 	event = service.events().insert(calendarId='primary', body=event).execute()
 	print('Event created: %s' % (event.get('htmlLink')))
-	print("meow")
+
 	print(event)
 
 
--- processText.py
+++ processText.py
@@ -1,4 +1,4 @@
-import re, logging, Image, pytesseract
+import re, logging, Image, pytesseract, datetime
 from PIL import Image
 logging.basicConfig(level=logging.DEBUG, format= "%(asctime)s - %(levelname)s - %(message)s")
 #print(pytesseract.image_to_string(Image.open('test.jpg')))
@@ -6,9 +6,38 @@ logging.basicConfig(level=logging.DEBUG, format= "%(asctime)s - %(levelname)s - 
 
 PlainText = pytesseract.image_to_string(Image.open('test.jpg'))
 
+event = {
+  'summary': 'I was unable to get a summary.',
+  'location': 'Unable to find a location',
+  'description': 'I was unable to find a description.',
+  'start': {
+    'dateTime': '1970-05-28T09:00:00-05:00',
+    'timeZone': 'America/Chicago',
+  },
+  'end': {
+    'dateTime': '1970-05-28T17:00:00-05:00',
+    'timeZone': 'America/Chicago',
+  },
+  'recurrence': [
+    'RRULE:FREQ=MONTHLY;COUNT=2'
+  ],
+  'attendees': [
+    {'email': 'lpage@example.com'},
+    {'email': 'sbrin@example.com'},
+  ],
+  'reminders': {
+    'useDefault': False,
+    'overrides': [
+      {'method': 'popup', 'minutes': 10},
+    ],
+  },
+}
+
+
 def processDate(data):
     copy_of_data = data
     print data
+    '''
     keywordRegex = re.compile(r'(date|time)*')
     matching_keyword_objects = keywordRegex.search(data)
     print matching_keyword_objects.group()
@@ -21,7 +50,26 @@ def processDate(data):
     print single_date
     
     # NEED TO CONVERT DATE/TIME INTO RFC3339 FORMAT
+    '''
+    dateRegex = re.compile(r'((?:(?<!\:)(?<!\:\d)[0-3]?\d(?:st|nd|rd|th)?\s+(?:of\s+)?(?:jan\.?|january|feb\.?|february|mar\.?|march|apr\.?|april|may|jun\.?|june|jul\.?|july|aug\.?|august|sep\.?|september|oct\.?|october|nov\.?|november|dec\.?|december)|(?:jan\.?|january|feb\.?|february|mar\.?|march|apr\.?|april|may|jun\.?|june|jul\.?|july|aug\.?|august|sep\.?|september|oct\.?|october|nov\.?|november|dec\.?|december)\s+(?<!\:)(?<!\:\d)[0-3]?\d(?:st|nd|rd|th)?)(?:\,)?\s*(?:\d{4})?|[0-3]?\d[-\./][0-3]?\d[-\./]\d{2,4})', re.IGNORECASE)
+    matching_date_list = dateRegex.findall(data)
+    print matching_date_list
+    single_date = matching_date_list[0].replace("\n", "")
+    print single_date
     
+    monthDict={1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun', 7:'Jul', 8:'Aug', 9:'Sep', 10:'Oct', 11:'Nov', 12:'Dec'}
+    monthDict = {"january":1, "february": 2, "march": 3, "april": 4, "may": 5, "june": 6, "july": 7, "august": 8, "september": 9, "october": 10, "november": 11, "december": 12}
+    split_date = single_date.split()
+    date = split_date[1]
+    month = monthDict[split_date[0].lower()]
+    
+    # single value months (0-9)
+    if len(str(month)) < 2:
+        month = '0' + str(month)
+    print "date", date
+    print "month", month
+    
+    '''
     # time regex
     timeRegex = re.compile(r'(\d.*(PM|AM))')
     
@@ -37,7 +85,15 @@ def processDate(data):
         startTime = time_span.split('')
     
     print time_span
+    '''
+    # time regex
+    timeRegex = re.compile(r'(\d{1,2}:\d{2} ?(?:[ap]\.?m\.?)?|\d[ap]\.?m\.?)', re.IGNORECASE)
     
+    time_span = timeRegex.findall(data)
+    
+    print "0"
+    print time_span
+
     
     
     # appropiate start and end times
@@ -51,27 +107,103 @@ def processDate(data):
     print startTime
     print endTime
     
+    # begin to convert to rfc 3339 format
+    # NEED TO ADD THE ABILITY TO HAVE MINUTES IN THE PROCESSING (SPLIT ON :)
+    if 'pm' in startTime.lower():
+
+        initial_time = int(startTime.lower().replace("pm", ""))
+        initial_time = (initial_time + 12) % 24
+        
+        # single value months (0-9)
+        if len(str(initial_time)) < 2:
+            month = '0' + str(initial_time)
+        print initial_time
+        #initial_time_number = int(initial_time.split())
+        
+    elif 'am' in startTime.lower():
+        initial_time = startTime.lower().replace("am", "")
+        # single value months (0-9)
+        if len(str(initial_time)) < 2:
+            month = '0' + str(initial_time)
+        print initial_time
+        
+    else:
+        pass
     
+    # end time
+    if 'pm' in endTime.lower():
 
-#processDate(PlainText)
+        end_time = int(endTime.lower().replace("pm", ""))
+        end_time = (end_time + 12) % 24
+        
+        # single value months (0-9)
+        if len(str(end_time)) < 2:
+            month = '0' + str(end_time)
+        print end_time
+        #end_time_number = int(end_time.split())
+        
+    elif 'am' in endTime.lower():
+        end_time = endTime.lower().replace("am", "")
+        # single value months (0-9)
+        if len(str(end_time)) < 2:
+            month = '0' + str(end_time)
+        print end_time
+        
+    else:
+        pass
+    
+    
+    
+    # hardcoded the year
+    # year, month, day, time, minute, month
+    try:
+        start_date_rfc = datetime.datetime(2016, int(month), int(date), int(initial_time), 00, 00).strftime("%Y-%m-%dT%I:%M:%S-05:00")
+    except:
+        start_date_rfc = datetime.datetime(2016, int(month), int(date), int(initial_time), 00, 00).strftime("%Y-%m-%dT%I:%M:%S-05:00")
+        
+    
+    print(start_date_rfc) 
+    
+    #2016-05-28T09:00:00-05:00
+    
+    # end date
+    try:
+        end_date_rfc = datetime.datetime(2016, int(month),int(date), int(end_time), 00, 00).strftime("%Y-%m-%dT%I:%M:%S-05:00")
+    except:
+        end_date_rfc = datetime.datetime(2016, int(month),int(date), int(end_time), 00, 00).strftime("%Y-%m-%dT%I:%M:%S-05:00")
+    
+    print end_date_rfc
+    
+    event['start']['dateTime'] = start_date_rfc
+    event['end']['dateTime'] = end_date_rfc
+    
+def processDescription(data):
+    numberRegex = re.compile(r'(((?:(?<![\d-])(?:\+?\d{1,3}[-.\s*]?)?(?:\(?\d{3}\)?[-.\s*]?)?\d{3}[-.\s*]?\d{4}(?![\d-]))|(?:(?<![\d-])(?:(?:\(\+?\d{2}\))|(?:\+?\d{2}))\s*\d{2}\s*\d{3}\s*\d{4}(?![\d-]))))', re.IGNORECASE)
+    matching_number_list = numberRegex.findall(data)
+    print matching_number_list
+    event['description'] = matching_number_list[0][0]
 
-
-
-
-
-def processText(PlainText):
-    pass
-    # date = processDate(PlainText)
-    # location = processLocation(PlainText)
-    # title = processTitle(PlainText)
-    # description  = processDescription(PlainText)
-   
+  
 def processLocation(data):
+    addressRegex = re.compile(r'(\d{1,4}[\w\s]{1,20}(?:street|st|avenue|ave|road|rd|highway|hwy|square|sq|trail|trl|drive|dr|court|ct|parkway|pkwy|circle|cir|boulevard|blvd)?.*[0-9]{5}(-[0-9]{4})?)', re.IGNORECASE)
+    matching_address_list = addressRegex.findall(data)
+    print data
+    print 'lol'
+    print matching_address_list
+    event['location'] = matching_address_list[0][0]
+    
+    
+# processLocation(PlainText)
+
+def processTitle(data):
     pass
 
 
-def addEvent():
-    pass
+
+
+
+print event
+
 
 
 
